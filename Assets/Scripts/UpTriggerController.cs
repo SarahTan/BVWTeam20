c9ea4859bc3248a5291df4493d9d8db69b19e446
Sarah <sarahtan@cmu.edu>
2015-09-30 18:06:45 -0400

    Added player controls/movement

---
+++ Assets/Scripts/UpTriggerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpTriggerController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.black;
+		Gizmos.DrawWireCube (transform.position, transform.localScale);
+	}
+	
+}
