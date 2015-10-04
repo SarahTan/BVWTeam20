831da43192023a12d4bb51400362b9f4196051ec
Sarah <sarahtan@cmu.edu>
2015-10-04 00:09:22 -0400

    Trying to fix animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,13 @@ public class GameManager : MonoBehaviour {
 
 	public Image fadeToBlack;
 	public GameObject player;
-	public GameObject startPos;
+	public GameObject startPos;	
+	public GameObject endPos;
 	public GameObject ovrCam;
 	public SoundManager soundManager;
-	
+
+	int endPosition;
+
 	// Use this for initialization
 	void Start () {
 		soundManager.mainThemeStart ();
@@ -62,9 +65,11 @@ public class GameManager : MonoBehaviour {
 		soundManager.raceThemeStart ();
 	}
 
-	public void RaceEnd () {
+	public void RaceEnd (int position) {
+		Debug.Log ("Ending Game");
 		StopAllCoroutines();
-		StartCoroutine("RaceEnd");
+		endPosition = position;
+		StartCoroutine("RaceEndSeq");
 	}
 
 	IEnumerator RaceEndSeq () {
@@ -75,6 +80,11 @@ public class GameManager : MonoBehaviour {
 		}
 
 		// move player to end spot
+		player.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		player.transform.position = endPos.transform.position;
+		//gameOverText.enabled = true;
+		//Text txt = GameObject.FindGameObjectWithTag ("GameOverText");
+		//txt.text += endPosition;
 
 		while (fadeToBlack.color.a > 0.05f) {
 			fadeToBlack.color = Color.Lerp (fadeToBlack.color, Color.clear, 1.5f*Time.deltaTime);
