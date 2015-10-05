d13c8f9e2508f86cd64015c8d14de23376d73af8
sarvesh <sarveshsvaran@gmail.com>
2015-10-05 01:03:02 -0400

    Updating the speed line density

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class ScoreKeeper : MonoBehaviour {
 	public GameObject speedline;
-
 	// Use this for initialization
 	void Start () {
-		//speedline.SetActive (false);
 		speedline.transform.GetChild (0).GetComponent<ParticleSystem> ().startSpeed = 7;
 		speedline.transform.GetChild (0).GetComponent<ParticleSystem> ().maxParticles = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (gameObject.GetComponent<Rigidbody> ().velocity.magnitude > 0) {
+			speedline.SetActive(true);
+		}
+		else
+			speedline.SetActive(false);
 	
 	}
 	void OnTriggerEnter(Collider other)
