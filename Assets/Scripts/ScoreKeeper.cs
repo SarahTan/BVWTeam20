89c21fe47c89c4b4395674e2209c0e6a0e20f6e4
sarvesh <sarveshsvaran@gmail.com>
2015-10-05 00:12:29 -0400

    Adding Coin Picking and speed line spawning

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	public GameObject speedline;
+
+	// Use this for initialization
+	void Start () {
+		speedline.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Orb") {
+			Destroy(other.gameObject);
+			StartCoroutine("Speedup");
+		}
+	}
+	IEnumerator Speedup()
+	{
+		speedline.SetActive (true);
+		yield return new WaitForSeconds(2);
+		speedline.SetActive (false);
+	}
+
+}
